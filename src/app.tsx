0a6fa5f2559f40cbab8b5b753d83602bd36041e3
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2024-04-25 16:36:54 -0300

    Functions recording and stopping notes, deleting notes, and application responsiveness

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useState } from 'react'
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
+import { toast } from 'sonner'
 
 interface Note{
   id: string
@@ -34,6 +35,16 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string){
+    const notesArray = notes.filter(note => {
+      return note.id != id
+    })
+
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+    toast.success('Nota deletada com sucesso!')
+  }
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>){
     const query = event.target.value
     setSearch(query)
@@ -42,7 +53,7 @@ export function App() {
   const filteredNotes = search != '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes
 
   return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
       <img src={logo} alt="NLW Expert" className='w-40' />
       <form className='w-full'>
         <input type="text" placeholder='Busque em suas notas...' className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500' onChange={handleSearch}/>
@@ -50,11 +61,11 @@ export function App() {
 
       <div className='h-px bg-slate-700'/>
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onNoteCreated={onNoteCreated} />
         
         {filteredNotes.map(note => {
-          return <NoteCard key={note.id} note={note}/>
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>
         })}
       </div>
     </div>
